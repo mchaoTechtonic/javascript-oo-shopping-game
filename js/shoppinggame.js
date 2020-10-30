0206b843dc9126f58231c8a9ca5833faedd0fa19
Matthew Chao <matthewchao@techtonic.com>
2020-10-30 16:25:45 -0400

    complete module 3

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -262,10 +262,14 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+  return (product) => product.id === id;
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+  return Math.floor(Math.random() * 20) + 1;
+};
 
 const getProduct = (prodList, pId) => {
   return prodList.find(findProductById(pId));
