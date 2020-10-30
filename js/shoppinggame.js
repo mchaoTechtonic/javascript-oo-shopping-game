e6cfed767193688a6d551af5c94a829118d276ef
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-30 09:29:10 -0400

    add method to product prototype

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -58,6 +58,9 @@ Object.defineProperty(
 )
 
 // Add method getDetails to Product here
+Product.prototype.getDetails = function () {
+    return `Product Name: ${this.name} , Product Price: ${this.price}`;
+}
 
 // Define the MagicProduct class here
 
